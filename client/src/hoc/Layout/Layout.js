f62d46ac2327707bf680b5307cb04336c0755b53
Casper Feng <casperf@stud.ntnu.no>
2019-02-21 12:26:49 +0100

    feat/aboutus

--- client/src/hoc/Layout/Layout.js
+++ client/src/hoc/Layout/Layout.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import AboutUs from '../../components/AboutUs/AboutUs';
+<<<<<<< HEAD
+=======
 import Header from '../../components/Header/Header';
+>>>>>>> b29258026ea5d20ccac3600f63d9f241f62a74eb
 
 
 class Layout extends Component {
@@ -10,7 +13,10 @@ class Layout extends Component {
           <React.Fragment>
              <Toolbar />
                 <main className="Content">
+<<<<<<< HEAD
+=======
                 <Header />
+>>>>>>> b29258026ea5d20ccac3600f63d9f241f62a74eb
                 <AboutUs />
                 </main>
           </React.Fragment>
