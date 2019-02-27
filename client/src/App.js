e53c52e45464d47258dbae879784b5c716879b6d
sigursl <sigursl@stud.ntnu.no>
2019-02-27 15:01:33 +0100

    Get better folder structure, and functional database connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
-import Layout from './hoc/Layout/Layout';
+import React, { Component } from "react";
+import "./App.css";
+import Layout from "./hoc/Layout/Layout";
 
 //Main site of the application, this is where we import other components
 
 class App extends Component {
-state = {
+  state = {
     data: null
   };
 
   componentDidMount() {
-      // Call our fetch function below once the component mounts
+    // Call our fetch function below once the component mounts
     this.callBackendAPI()
       .then(res => this.setState({ data: res.express }))
       .catch(err => console.log(err));
   }
-    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
   callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
+    const response = await fetch("/api/users");
     const body = await response.json();
+    console.log(body);
 
     if (response.status !== 200) {
-      throw Error(body.message) 
+      throw Error(body.message);
     }
     return body;
   };
@@ -29,8 +30,7 @@ state = {
   render() {
     return (
       <React.Fragment>
-        <Layout>
-        </Layout>
+        <Layout />
       </React.Fragment>
     );
   }
