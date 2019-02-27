6ca9f004a90b91bf8a64676ae32821eda113bb53
unknown <erikhsa@stud.ntnu.no>
2019-02-27 13:31:56 +0100

    Fixed css file for login page

--- client/src/components/LoginPage/LoginPage.js
+++ client/src/components/LoginPage/LoginPage.js
@@ -5,15 +5,15 @@ const loginpage = (props) => (
     <div className="loginpage">
         <h1>Logg inn</h1>
         <form>
-            <label>Brukernavn:</label>
+            <label class="loginlabel">Brukernavn:</label>
             <br/>
-            <input type="text" name="username" />
+            <input class="logininput" type="text" name="username" />
             <br/>
-            <label>Passord:</label>
+            <label class="loginlabel">Passord:</label>
             <br/>
-            <input type="text" name="password" />
+            <input class="logininput" type="text" name="password" />
             <br/>
-            <button>Logg inn</button>
+            <button class="loginbutton">Logg inn</button>
         </form>
     </div>
 )
