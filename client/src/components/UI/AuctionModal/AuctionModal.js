4f5b8a959413538c99148b6ecfd10cc85259bad4
sigursl <sigursl@stud.ntnu.no>
2019-02-28 15:53:58 +0100

    fix variablenaming in Aunctionmodal.js

--- client/src/components/UI/AuctionModal/AuctionModal.js
+++ client/src/components/UI/AuctionModal/AuctionModal.js
@@ -1,54 +1,82 @@
-import React, {Component} from   'react';
-import { runInThisContext } from 'vm';
+import React, { Component } from "react";
+import { runInThisContext } from "vm";
 
 class AuctionModal extends Component {
   //does not need any info
   state = {
-    name: '',
-    desc: '',
-    picture: '',
-    lowestBid: '',
-    highestBid: '', 
+    title: "",
+    desc: "",
+    image: "",
+    startingBid: "",
+    highestBid: ""
   };
 
-  handleNameChange = (e) => {
-    this.setState( {name: e.target.value} );
-  }
+  handleNameChange = e => {
+    this.setState({ title: e.target.value });
+  };
 
-  handleDescChange = (e) => {
-    this.setState( {desc: e.target.value} );
-  }
+  handleDescChange = e => {
+    this.setState({ desc: e.target.value });
+  };
 
-  handlePictureChange = (e) => {
-    this.setState( {picture: e.target.value} );
-  }
+  handlePictureChange = e => {
+    this.setState({ image: e.target.value });
+  };
 
-  handleBidChange = (e) => {
-    this.setState( {lowestBid: e.target.value} )
-  }
+  handleBidChange = e => {
+    this.setState({ startingBid: e.target.value });
+  };
 
-  handleSubmit = (e) => {
+  handleSubmit = e => {
     e.preventDefault();
-    const name = this.state.name;
+    const title = this.state.title;
     const desc = this.state.desc;
-    const picture = this.state.picture;
-    const bid = this.state.lowestBid;
-    const auc = {id: '123', 'name': name, 'description': desc, 'picture': picture, lowestBid: bid, highestBid: bid};
+    const image = this.state.image;
+    const bid = this.state.startingBid;
+    const auc = {
+      id: "123",
+      title: title,
+      description: desc,
+      image: image,
+      startingBid: bid,
+      highestBid: bid
+    };
     this.props.submit(auc);
-  }
+  };
 
-  render(){
-    return(
+  render() {
+    return (
       <React.Fragment>
-    <h4>Skriv inn info om produktet</h4>
+        <h4>Skriv inn info om produktet</h4>
         <form>
-          <input type="text" name="navn" placeholder="Navn" onChange={this.handleNameChange}/>
-          <input type="text" name="beskrivelse" placeholder="Beskrivelse" onChange={this.handleDescChange}/>
-          <input type="text" name="bilde" placeholder="Bilde her..." onChange={this.handlePictureChange} />
-          <input type="number" name="minstebud" placeholder="Startbud" min="1" onChange={this.handleBidChange}/>
-         <button onClick={this.handleSubmit}>Submit</button>
-          </form>
-    </React.Fragment>
+          <input
+            type="text"
+            title="navn"
+            placeholder="Navn"
+            onChange={this.handleNameChange}
+          />
+          <input
+            type="text"
+            title="beskrivelse"
+            placeholder="Beskrivelse"
+            onChange={this.handleDescChange}
+          />
+          <input
+            type="text"
+            title="bilde"
+            placeholder="Bilde her..."
+            onChange={this.handlePictureChange}
+          />
+          <input
+            type="number"
+            title="minstebud"
+            placeholder="Startbud"
+            min="1"
+            onChange={this.handleBidChange}
+          />
+          <button onClick={this.handleSubmit}>Submit</button>
+        </form>
+      </React.Fragment>
     );
   }
 }
