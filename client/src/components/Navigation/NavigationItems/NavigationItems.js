6faaf265e9bf4391f681ea5190eab9cbe91d659b
Casperfeng <casp.feng@gmail.com>
2019-03-03 21:09:12 +0100

    Remove duplicate navigationelement

--- client/src/components/Navigation/NavigationItems/NavigationItems.js
+++ client/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,7 +7,6 @@ const navigationItems = () => (
     <ul className={"NavigationItems"}>
         <NavigationItem link="/"><span style={{marginRight: '30px', marginLeft: '30px', fontSize: '16px'}}>Home</span></NavigationItem>
         <NavigationItem link="/auctions"><span style={{marginRight: '30px', marginLeft: '30px', fontSize: '16px'}}>Auksjoner </span></NavigationItem>
-        <NavigationItem link="/"><span style={{marginRight: '30px', marginLeft: '30px', fontSize: '16px'}}>Om oss</span></NavigationItem>
         <NavigationItem link="/contact"><span style={{marginRight: '30px', marginLeft: '30px', fontSize: '16px'}}>Kontakt</span></NavigationItem>
         {localStorage.getItem("token") === null ? <NavigationItem link="/login"><span style={{marginRight: '30px', marginLeft: '30px', fontSize: '16px'}}>Logg inn</span></NavigationItem> : <NavigationItem link="/logout"><span style={{marginRight: '30px', marginLeft: '30px', fontSize: '16px'}}>Logg ut</span></NavigationItem>}
     </ul>
