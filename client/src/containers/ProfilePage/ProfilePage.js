53820b1d80e59d6cf5aa20b42c93c703483ab46a
Casperfeng <casp.feng@gmail.com>
2019-03-20 14:56:23 +0100

    Add dummy description of trsnsaction object

--- client/src/containers/ProfilePage/ProfilePage.js
+++ client/src/containers/ProfilePage/ProfilePage.js
@@ -3,14 +3,19 @@ import jwtDecode from "jwt-decode";
 import './ProfilePage.css';
 import profile from '../../assets/placeholder.jpg' ;
 import PropTypes from "prop-types";
+import Transactions from './Transactions/Transactions';
 class ProfilePage extends Component {
- state = { };
+ 
+  state = { usr: {},
+    transactionsClicked: false,
+    
+  };
 
   componentWillMount = () => {
    try {
     const jwt = localStorage.getItem("token"); //check if token exists
     const user = jwtDecode(jwt); //decode token
-    this.setState({ user }); //webtoken exist, logged in
+    this.setState({ usr:user }); //webtoken exist, logged in
    } catch (ex) {
      //webtoken do not exist, logged out
    }
@@ -23,20 +28,19 @@ class ProfilePage extends Component {
          <img className="profile--image" src={profile} alt="Placeholder"/>
        </div>
        <div id="details">
-         <h2>{this.state.user.firstName + " " + this.state.user.lastName}</h2>
-         <h2>{this.state.phoneNumber}</h2>
+         <h2>{this.state.usr.firstName + " " + this.state.usr.lastName}</h2>
+         <h2>{this.state.usr.phoneNumber}</h2>
        </div>
 
        <div className="profile container grid-2">
          <div>
-           <h4><a href="#">Mine handler</a></h4>
-           <h4><a href="#">Meldinger</a></h4>
-           <h4><a href="#">Vurderinger</a></h4>
+           <h4 onClick={() => this.setState({transactionsClicked: !this.state.transactionsClicked} )}className="profileOption">Mine handler</h4>
+           {this.state.transactionsClicked ? <Transactions /> : null}
+           <h4 className="profileOption">Vurderinger</h4>
          </div>
          <div>
-           <h4><a href="#">Profil</a></h4>
-           <h4><a href="#">Endre profil</a></h4>
-           <h4><a href="#">Slett konto</a></h4>
+           <h4 className="profileOption">Endre profil</h4>
+           <h4 className="profileOption">Slett konto</h4>
          </div>
        </div>
      </div>
---
+++ client/src/containers/ProfilePage/Transactions/Transactions.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './Transactions.css';
+
+import Transaction from './Transaction/Transaction';
+
+class Transactions extends Component {
+
+  state = {
+    transactions: [],
+    /**
+     * Dette er en DUMMY-transaksjon
+     * {buyerId: buyerID (navn), sellerId: sellerID (navn), auctionItem: (navnet til gjenstanden),  soldItem: (var dette et kjøp eller salg?, true/false), price: (prisen gjenstanden ble solgt for)  }
+     */
+  };
+  
+  componentDidMount = () => {
+   //henter transaksjoner for brukeren ved et endpoint som JSON-objekter med ID hentet fra json-token
+    let userTransactions;
+   this.setState({transactions: userTransactions});
+  }
+
+  render() {
+
+    const personalTransactions = this.state.transactions.map( (transaction) => (
+      <li className="transactionItem"><Transaction /></li>  
+    ));
+
+    return (
+      <div>
+        <ul>
+        {personalTransactions}
+        </ul>         
+      </div>
+    );
+  }
+}
+
+export default Transactions;
