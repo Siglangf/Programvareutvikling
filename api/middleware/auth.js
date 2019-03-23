a7d80716c05e3af4b55ad2a19ad4fbceeb7c5710
sigursl <sigursl@stud.ntnu.no>
2019-03-23 16:22:55 +0100

    remove console.logs

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = function(req, res, next) {
   const token = req.header("x-auth-token");
-  console.log(token);
+
   if (!token) return res.status(401).send("Access denied. No token provided");
   try {
     const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
